0f007f762d1e8581035ddb8dac9afcdb29811d13
Alexander Radyushin <r.alex.andr@ya.ru>
2020-11-03 09:19:03 +0300

    Updated tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,16 @@
+import { redis } from '@fjedi/redis-client';
 import { getTableName } from '../src';
 
 describe('Test database-client', function () {
+  beforeAll(async () => {
+    console.log('Emit beforeAll test-hook');
+  });
+
+  afterAll(async () => {
+    console.log('Emit afterAll test-hook');
+    redis.end(true);
+  });
+
   it('Get valid snake-cased db-table name', async function () {
     const result = getTableName('UserSession');
 
